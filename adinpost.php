eb48b1eef72f176945c79eda48e3093fb4615582
Alexandre Vaillant <vaillant.alex@yahoo.fr>
2016-10-18 09:49:38 +0200

    Formulaire BO
    Création du formulaire en backoffice (non fonctionnel pour le moment)

--- adinpost.php
+++ adinpost.php
@@ -3,7 +3,7 @@
 Plugin Name: Adinpost
 Plugin URI: https://github.com/vailalex/AV_Workshop
 Description: devoir à rendre pour le 21/10
-Version: 0.16
+Version: 0.17
 Author: Alexandre Vaillant 3adev
 */
 
@@ -28,14 +28,61 @@ add_action('admin_menu', 'add_admin_menu');
 
 function add_admin_menu()
 {					/*titre de la page, titre du menu, droit, */
-	    add_menu_page('Options Adinpost', 'Adinpost', 'manage_options', 'av_bdc', 'menu_html');
+	    add_menu_page('Options Adinpost', 'Adinpost', 'manage_options', 'adinpost', 'menu_html');
+
+
+	    add_action('admin_init', 'av_register_settings');
+
+		
+		
+		/*valeur par défaut du champ de bas de page*/
+		$valeur_option = get_option(av_bas_de_page);
+		if($valeur_option==NULL){
+		
+			update_option ('av_bas_de_page', __('Text limited to 300 letters.', 'av_bas_de_contenu'));
+		}
 }
+
+
+function av_register_settings()
+{
+    register_setting('av_groupe_options', 'av_bas_de_page');
+
+}
+
+
+/*BO formulaire*/
 
 function menu_html()
 {
+	?>
+	
+	<div id="av_formu">
+	<?php
     echo '<h1>'.get_admin_page_title().'</h1>';
-    echo '<p>Bienvenue sur la page d\'accueil du plugin</p>';
+    _e('Please write your text :', 'av_bas_de_contenu');
+	
+		?>
+	
+	 <form id="formulaire" method="post" action="options.php">
+	 
+		<?php settings_fields('av_groupe_options'); ?>
+		<?php do_settings_sections('av_bas_de_page'); ?>
+		<textarea maxlength="300" rows="4" cols="50" type="text" value="av_form" name="av_bas_de_page"><?php _e(get_option('av_bas_de_page', 'av_bas_de_contenu'));?></textarea>	
+		<?php submit_button(); ?>
+	
+    </form>
+
+	</div>
+
+
+
+	<?php
+	
 }
+
+
+
 
 /*bouton barre horizontale admin */
 
@@ -43,7 +90,7 @@ add_action('add_admin_bar_menus', 'av_admin_bar');
 
 function av_admin_bar_down() {
 	global $wp_admin_bar;
-	$wp_admin_bar->add_menu( array( 'id' => 'bas_de_contenu', 'title' => __('Adinpost', 'av_bas_de_contenu'), 'href' => admin_url( 'admin.php?page=av_bdc' ) ) );
+	$wp_admin_bar->add_menu( array( 'id' => 'bas_de_contenu', 'title' => __('Adinpost', 'av_bas_de_contenu'), 'href' => admin_url( 'admin.php?page=adinpost' ) ) );
 }
 
 function av_admin_bar() {
