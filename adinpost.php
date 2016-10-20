d9e0d465ce7c96ee4397982eb493af821815742d
Alexandre Vaillant <vaillant.alex@yahoo.fr>
2016-10-21 00:49:21 +0200

    version définitive (ajout de sécurité)
    A faire :
    -Traduction
    -Filtre par catégories
    -Images

--- adinpost.php
+++ adinpost.php
@@ -8,19 +8,28 @@ Author: Alexandre Vaillant 3adev
 */
 
 
+/*traduction fr*/
 
-/* Affichage du bas de contenu, protection contre faille xss avec wp_kses (autorisation des lien href) (faire dans la prochaine version) */
+add_action( 'plugins_loaded', 'av_traduction' );
+
+
+function av_traduction() {
+  load_plugin_textdomain( 'av_bas_de_contenu', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' ); 
+}
+
+
+/* Affichage du bas de contenu, protection contre faille xss avec wp_kses (autorisation des lien href) */
 
 function av_ajout_texte($contenu){
-		$mon_bas_de_contenu= get_option("av_bas_de_page");
+		$mon_bas_de_contenu= wp_kses( get_option("av_bas_de_page"), array( 'a' => array( 'href' => array() ) ) );
 		$bas_de_contenu = "<div id='bas_de_contenu'>" . $mon_bas_de_contenu . "</div> ";
 		$nouveau_contenu = $contenu . $bas_de_contenu;
 		return $nouveau_contenu;
-		
+	
 }
 
 function av_ajout_texte2($contenu){
-		$mon_haut_de_contenu= get_option("av_haut_de_page");
+		$mon_haut_de_contenu= wp_kses( get_option("av_haut_de_page"), array( 'a' => array( 'href' => array() ) ) );
 		$haut_de_contenu = "<div id='haut_de_contenu'>" . $mon_haut_de_contenu . "</div><br> ";
 		$nouveau_contenu = $haut_de_contenu . $contenu;
 		return $nouveau_contenu;
@@ -36,12 +45,13 @@ add_filter( 'the_content', 'av_ajout_texte');
 add_action('admin_menu', 'add_admin_menu');
 
 function add_admin_menu()
-{					/*titre de la page, titre du menu, droit, */
+{					
+	/*titre de la page, titre du menu, droit, */
 	    add_menu_page('Options Adinpost', 'Adinpost', 'manage_options', 'adinpost', 'menu_html');
 
 
 	    add_action('admin_init', 'av_register_settings');
-
+	     
 	
 		
 		/*valeur par défaut du champ de bas de page*/
@@ -58,31 +68,53 @@ function av_register_settings()
 {
     register_setting('av_groupe_options', 'av_bas_de_page');
     register_setting('av_groupe_options', 'av_haut_de_page');
-}
 
+}
+/*Bloc category ne marche pas
+
+
+
+	$cats= get_categories();
+
+
+	if ($cats) 
+	{
+    foreach ( $cats as $cat ) 
+    	{
+    		$p = 'av_bas_de_page_'+$cat->name;
+    	var_dump($p);
+    		register_setting('av_groupe_options', 'av_bas_de_page_'+$cat->name);
+    		register_setting('av_groupe_options', 'av_haut_de_page_'+$cat->name);
+    	}
+	}
+}
+*/
 
 /*bouton barre horizontale admin */
 
 add_action('add_admin_bar_menus', 'av_admin_bar');
 
-function av_admin_bar_down() {
+function av_admin_bar_down() 
+{
 	global $wp_admin_bar;
 	$wp_admin_bar->add_menu( array( 'id' => 'bas_de_contenu', 'title' => __('Adinpost', 'av_bas_de_contenu'), 'href' => admin_url( 'admin.php?page=adinpost' ) ) );
 }
 
-function av_admin_bar() {
+function av_admin_bar() 
+{
 add_action( 'admin_bar_menu', 'av_admin_bar_down', 40 );	
 }
 
 /*admin icon*/
 
 add_action( 'admin_head', 'av_admin_menu_icon' );
-function av_admin_menu_icon() {
+function av_admin_menu_icon() 
+{
 ?>
 
-<style type="text/css">
-.dashicons-admin-generic:before {
-content: "\f209";
+	<style type="text/css">
+	.dashicons-admin-generic:before {
+	content: "\f209";
 }
 </style>
 
@@ -94,28 +126,42 @@ content: "\f209";
 
 function menu_html()
 {
-
 	?>
-	
-
 	<div id="av_formu">
 	<?php
-	    echo '<h1>'.get_admin_page_title().'</h1><br>';
-
-
-		?>
+	echo '<h1>'.get_admin_page_title().'</h1><br>';
+	?>
           
-
-       
-
-	
-	 <form id="formulaire" method="post" action="options.php">
+	<form id="formulaire" method="post" action="options.php" enctype="multipart/form-data">
 	 
 
-<br>
-		<?php settings_fields('av_groupe_options'); ?>
+	<select class="form-control" id="sel1" name="filtre">
 
-		<?php do_settings_sections('av_haut_de_page', 'av_bas_de_page'); ?>
+	<option>Total</option>
+	<?php
+	$args = array( 'numberposts' => -1); 
+	$cats= get_categories();
+
+
+	if ($cats) 
+	{
+    	foreach ( $cats as $cat ) 
+    	{
+    		echo "<option>".$cat->name."</option>";
+    	}
+	}
+	
+	?>
+	</select>
+
+
+    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+
+	<?php
+	settings_fields('av_groupe_options'); 
+	do_settings_sections('av_haut_de_page', 'av_bas_de_page'); 
+	?>
+
 		<textarea maxlength="300" rows="4" cols="50" type="text" placeholder="Please write your header content" value="av_form" name="av_haut_de_page"><?php _e(get_option('av_haut_de_page', 'av_haut_de_contenu'));?></textarea>	
 		 
 		<textarea maxlength="300" rows="4" cols="50" type="text" placeholder="Please write your footer content" value="av_form" name="av_bas_de_page"><?php _e(get_option('av_bas_de_page', 'av_bas_de_contenu'));?></textarea>	
@@ -128,21 +174,25 @@ function menu_html()
 	</div>
 
 	<?php
+
 }
 
 /*confirmation */
 
 add_action('admin_notices', 'av_admin_notice');
 
-function av_admin_notice() {
-   if( isset($_GET['settings-updated']) ) { ?>
-    <div id="message" class="updated notice notice-success is-dismissible below-h2"> <!-- Css établis en dehors du Plugin et par Wordpress --> 
-	<p><?php _e('Updated', 'av_bas_de_contenu')?> <a href="/wordpress/index.php/"><?php _e('Back to the site', 'av_bas_de_contenu')?></a></p>
-	<button type="button" class="notice-dismiss"></button>
+function av_admin_notice() 
+{
+   if( isset($_GET['settings-updated']) ) 
+   	{ ?>
+    	<div id="message" class="updated notice notice-success is-dismissible below-h2"> <!-- Css établis en dehors du Plugin et par Wordpress --> 
+		<p><?php _e('Updated', 'av_bas_de_contenu')?> <a href="/wordpress/index.php/"><?php _e('Back to the site', 'av_bas_de_contenu')?></a></p>
+		<button type="button" class="notice-dismiss"></button>
     </div>
    <?php
    }
 }
+
 
 /* css site */
 
@@ -154,7 +204,10 @@ function av_appel_style() {
 
 add_action( 'wp_enqueue_scripts', 'av_appel_style' );
 */
+
+
 ?>
+
 
 <style>
 
